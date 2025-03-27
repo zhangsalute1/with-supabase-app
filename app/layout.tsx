a4acbd0a2eed654157a55a747b0275a93af62c5e
zhangsalute1 <zhangsalute1@gmail.com>
2025-03-27 17:27:55 +0800

    supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,9 @@ export default function RootLayout({
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
+                    {/* <div className="flex items-center gap-2">
                       <DeployButton />
-                    </div>
+                    </div> */}
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
